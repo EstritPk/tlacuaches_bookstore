4505251d4a4ead095cb3cca57742a826e629b5b2
EstritPk <87834549+EstritPk@users.noreply.github.com>
2023-04-23 00:14:30 -0400

    Add files via upload

---
+++ add.php
@@ -0,0 +1,98 @@
+<?php
+
+  require "database.php";
+
+  if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $nameBook = $_POST["name_book"];
+    $authorBook = $_POST["author_book"];
+
+    $statement = $conn->prepare("INSERT INTO books (name_book, author_book) ")
+  
+    header("Location: index.php");
+  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- bootstrap -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css" integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+
+  <!-- Static Content -->
+  <link rel="stylesheet" href="./static/css/index.css" />
+
+  <title>Libreria Tlacuaches</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand font-weight-bold" href="#">
+          <img class="mr-2" src="./static/img/logo.png" />
+          LibreriaTlacuaches
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="./index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="add.html">Agregar Libro</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+  </nav>
+
+  <main>
+    <div class="container pt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <div class="card">
+            <div class="card-header">Agregar Nuevo Libro</div>
+            <div class="card-body">
+              <form method="POST" action="add.php">
+                <div class="mb-3 row">
+                  <label for="name" class="col-md-4 col-form-label text-md-end">Nombre</label>
+                  <div class="col-md-6">
+                    <input id="name_book" type="text" class="form-control" name="name_book" required autocomplete="name_book" autofocus>
+                  </div>
+                </div>
+
+                <div class="mb-3 row">
+                  <label for="author_book" class="col-md-4 col-form-label text-md-end">Autor</label>
+                  <div class="col-md-6">
+                    <input id="author_book" type="tel" class="form-control" name="author_book" required autocomplete="author_book" autofocus>
+                  </div>
+                </div>
+
+                <div class="mb-3 row">
+                  <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">Agregar</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+</body>
+</html>
